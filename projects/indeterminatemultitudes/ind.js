2b94927296d19e2daf063fd4ac4adc7a2cb571a9
Cha <71788879+chhaski@users.noreply.github.com>
2023-11-20 21:14:06 -0500

    Index doc updates

--- projects/indeterminatemultitudes/ind.js
+++ projects/indeterminatemultitudes/ind.js
@@ -1,51 +1,35 @@
-// noprotect
+var hierarchy = document.getElementById("hierarchy");
+hierarchy.addEventListener("click", function(event){
+    var elem = event.target;
+    if(elem.tagName.toLowerCase() == "span" && elem !== event.currentTarget)
+    {
+        var type = elem.classList.contains("folder") ? "folder" : "file";
+        if(type=="file")
+        {
+            alert("File accessed");
+        }
+        if(type=="folder")
+        {
+            var isexpanded = elem.dataset.isexpanded=="true";
+            if(isexpanded)
+            {
+                elem.classList.remove("fa-folder-o");
+                elem.classList.add("fa-folder");
+            }
+            else
+            {
+                elem.classList.remove("fa-folder");
+                elem.classList.add("fa-folder-o");
+            }
+            elem.dataset.isexpanded = !isexpanded;
 
-var scl = 8;
-var xoffset, yoffset, zoffset = 0.0;
-var cols, rows;
-var alph = ["0","O","|",".","/", ":","╲"];
+            var toggleelems = [].slice.call(elem.parentElement.children);
+            var classnames = "file,foldercontainer,noitems".split(",");
 
-function preload() {
-  //myFont = loadFont('assets/inconsolata.otf');
-}
-
-function setup() {
-  background(255);
-
-  createCanvas(600, 600);
-
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-
-  noStroke();
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textFont('Helvetica');
-  textSize(scl);
-}
-
-function draw() {
-  background(255);
-
-  xoffset = 0;
-
-  for (var x = 0; x < cols; x++) {
-    yoffset = 0;
-    for (var y = 0; y < rows; y++) {
-      noiseDetail(2, 0.5);
-			var flow = noise(xoffset, yoffset, zoffset);
-      var index = round(map(flow, -1, 1, 0, alph.length));
-
-			push();
-    	translate(x * scl, y * scl);
-      text(alph[index], 0, 0, scl, scl);     
-      pop();
-
-      yoffset += 0.07;
+            toggleelems.forEach(function(element){
+                if(classnames.some(function(val){return element.classList.contains(val);}))
+                element.style.display = isexpanded ? "none":"block";
+            });
+        }
     }
-
-    xoffset += 0.05;
-  }
-
-  zoffset += 0.04;
-}
+});
